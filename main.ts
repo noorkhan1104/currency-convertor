282d81645548d2bcdc0ac87e0cd787de7e5cbf9a
noorkhan1104 <164567175+noorkhan1104@users.noreply.github.com>
2024-04-30 09:33:59 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,31 @@
+import inquirer from "inquirer";
+
+const currency ={
+    USD : 1,
+    EUR : 0.91,
+    GBP : 0.76,
+    IND :74.57,
+    PKR :280,
+};
+let user_answer = await inquirer.prompt(
+    [
+        {
+            name: "from,",
+            message :"enter from currency",
+            types :"list",
+            choices:["USD","EUR","GBP","IND","PKR"]
+        },
+        {
+            name: "to",
+            message :"enter to currency",
+            types :"list",
+            choices:["USD","EUR","GBP","IND","PKR"]
+        },
+        {
+            name: "currency",
+            message :"enter your amount",
+            types :"number",
+        }
+    ]
+)
+console.log(user_answer)
